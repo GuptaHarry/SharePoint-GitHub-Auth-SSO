80daaa02524ab1c0705379cfd7820b5839c55ab1
GuptaHarry <harikrishnagupta72@gmail.com>
2026-04-09 16:29:18 +0530

    Setup code added

---
+++ src/webparts/spfxGithubSignin/components/ISpfxGithubSigninProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxGithubSigninProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfxGithubSignin/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxGithubSigninWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxGithubSigninWebPartStrings' {
+  const strings: ISpfxGithubSigninWebPartStrings;
+  export = strings;
+}
