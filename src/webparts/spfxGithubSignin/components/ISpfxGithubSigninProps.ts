75b447d9dc5c552ce88a553098de8b29d4d1bd56
GuptaHarry <harikrishnagupta72@gmail.com>
2026-04-09 16:38:31 +0530

    Files ignored

--- src/webparts/spfxGithubSignin/components/ISpfxGithubSigninProps.ts
+++ src/webparts/spfxGithubSignin/components/ISpfxGithubSigninProps.ts
@@ -1,7 +1,4 @@
 export interface ISpfxGithubSigninProps {
   description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
+  
 }
